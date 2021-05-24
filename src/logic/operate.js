26fc414723b46ddf327fc1fd10b0d1ad8cef67ca
Henok <henatan99@gmail.com>
2021-05-24 19:36:57 +0300

    Add calculate functional component which accept two numbers and operation arguments, import big.js

---
+++ src/logic/operate.js
@@ -0,0 +1,22 @@
+import Big from 'big.js';
+
+function operate(numberOne, numberTwo, operation) {
+    const num1 = Big(numberOne);
+    const num2 = Big(numberTwo);
+    switch(operation) {
+        case '+': 
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case 'X':
+            return num1 * num2
+        case '÷':
+            return num1 / num2
+        case '%':
+            return num1 % num2
+        default:
+            return "Invalid operator"
+    }
+}
+
+export default operate
