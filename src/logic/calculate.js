c1c2d1a24618bd855cb9ccdb9d30b2102e21057f
Henok <henatan99@gmail.com>
2021-05-27 04:32:42 +0300

    Fix bug when number is clicked on an existing number history

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -7,7 +7,7 @@ function calculate(data, name) {
   const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
   
   if (nums.includes(name)) {
-    next = next ? next += name : name;
+    next = (next && next != history) ? next += name : name;
     history = (history == null) ? name : history + name; 
   }
   else if(ops.includes(name)) {
