8fb1eb7ced17a15af65bbe020b50ef3f9dd11d28
Henok <henatan99@gmail.com>
2021-06-01 01:37:38 +0300

    Refactor App component with react hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-      history: '',
-    };
+const App = () => {
+  const [data, setData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+    history: '',
+  });
+
+  const handleClick = (name) => {
+    const newData = calculate(data, name);
+    setData(() => ({ ...newData }));
   }
 
-  handleClick = (name) => {
-    const newState = calculate(this.state, name);
-    this.setState(() => ({ ...newState }));
-  }
-
-  render() {
-    const {
-      total, next, history,
-    } = this.state;
-    return (
-      <>
-        <Display
-          result={(next && next.toString()) || (total && total.toString()) || '0'}
-          process={history}
-        />
-        <ButtonPanel handleClick={this.handleClick} />
-      </>
-    );
-  }
+  return (
+    <>
+      <Display
+        result={(data.next && data.next.toString()) || (data.total && data.total.toString()) || '0'}
+        process={data.history}
+      />
+      <ButtonPanel handleClick={handleClick} />
+    </>
+  );
 }
 
 export default App;
