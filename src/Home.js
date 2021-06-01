af646dbeebe501519e6f8344155044966bd15db0
Henok <henatan99@gmail.com>
2021-06-01 19:02:59 +0300

    Add content and styling for Home page

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 function Home() {
     return (
-        <h1>Home Page</h1>
+        <section className="home">
+            <h1 className="home-title">Welcome to Our Page</h1>
+            <p className="par">
+                Math Magicians is a fictional company based in France. This website
+                is has a calculator app the company develops for users. It has also quotes 
+                which inspires users. There are links in the nav bar which helps 
+                you navigate through the different pages. Enjoy our website! 
+            </p>
+            <p className="par">
+                Math Magicians is a fictional company based in France. This website
+                is has a calculator app the company develops for users. It has also quotes 
+                which inspires users. There are links in the nav bar which helps 
+                you navigate through the different pages. Enjoy our website! 
+            </p>
+        </section>
     )
 }
 
