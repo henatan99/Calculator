d51b16c78d04a0e9094642926a564b6398f7c6bc
Henok <henatan99@gmail.com>
2021-06-01 19:25:39 +0300

    Add content and styling to Quotes page

--- src/Quotes.js
+++ src/Quotes.js
@@ -2,7 +2,9 @@ import React from'react'
 
 function Quotes() {
     return (
-        <h3>Qoutes Page</h3>
+        <h2 className="quote">Mathematics is not about numbers, equations, computations or
+            algorithms: it is about understanding. - William Paul Thurston 
+        </h2>
     )
 }
 
