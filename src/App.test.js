3d894984872121cd1ffbe49c6e52457e56f5b1e6
Henok <henatan99@gmail.com>
2021-06-02 16:47:06 +0300

    Add render and userEvent tests for the App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders the correct content', () => {
+  render(<App />)
+  const navLogo = screen.getByText('Math Magicians');
+  expect(navLogo).toBeInTheDocument();
+})
+
+test('renders the Home page when Home link clicked', () => {
+  render(<App />)
+  const leftClick = {button: 0}
+  userEvent.click(screen.getByText('Home'), leftClick)
+  expect(screen.getByText('Welcome to Our Page'))
+})
+
+test('renders the Calculator page when Calculator link clicked', () => {
+  render(<App />)
+  const leftClick = {button: 0}
+  userEvent.click(screen.getByText('Calculator'), leftClick)
+  expect(screen.getByText('Let\'s do Some Math!'))
+})
+
+test('renders the Quotes page when Quote link clicked', () => {
+  render(<App />)
+  const leftClick = {button: 0}
+  const quoteContent = 'Mathematics is not about numbers, equations, ' +  
+  'computations or algorithms: it is about understanding. - William Paul Thurston';
+  userEvent.click(screen.getByText('Quote'), leftClick)
+  expect(screen.getByText(quoteContent))
+})
